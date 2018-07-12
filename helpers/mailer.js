da504afabcb3609f8581db708795fcf911301924
nallelyht <nallelyht@gmail.com>
2018-07-12 11:38:44 -0500

    correo

---
+++ helpers/mailer.js
@@ -0,0 +1,21 @@
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.gmail,
+    pass: process.env.gmailPass 
+  }
+});
+
+exports.sendWelcomeMail = (user)=>{
+  const data = {
+    from: '"Hola 👻" <nallelyht@gmail.com>',
+    to: user.email, 
+    subject: `Hola ${user.name}`, 
+    text: 'Awesome Message',
+   // html: '<b>Awesome Message</b>'
+  }
+  transporter.sendMail(data)
+  .then(info => console.log(info))
+  .catch(error => console.log(error))
+}
