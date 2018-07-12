3d52f9f04a67d36e6d5cf4621fc8b84f300dc091
nallelyht <nallelyht@gmail.com>
2018-07-12 12:08:50 -0500

    correo

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -1,21 +1,23 @@
 const nodemailer = require('nodemailer');
+const hbs = require('hbs');
+const fs = require('fs');
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: process.env.gmail,
-    pass: process.env.gmailPass 
-  }
+	service: 'Gmail',
+	auth: {
+		user: process.env.gmail,
+		pass: process.env.gmailPass
+	}
 });
 
-exports.sendWelcomeMail = (user)=>{
-  const data = {
-    from: '"Hola 👻" <nallelyht@gmail.com>',
-    to: user.email, 
-    subject: `Hola ${user.name}`, 
-    text: 'Awesome Message',
-   // html: '<b>Awesome Message</b>'
-  }
-  transporter.sendMail(data)
-  .then(info => console.log(info))
-  .catch(error => console.log(error))
-}
+const wellcomeCompile = hbs.compile(fs.readFileSync((__dirname, '../views/welcome.hbs'), 'utf8'));
+
+exports.sendWelcomeMail = (user) => {
+	const data = {
+		from: '"Hola 👻" <nallelyht@gmail.com>',
+		to: user.email,
+		subject: `Hola ${user.name}`,
+		//text: 'Awesome Message'
+		html: wellcomeCompile(user)
+	};
+	transporter.sendMail(data).then((info) => console.log(info)).catch((error) => console.log(error));
+};
